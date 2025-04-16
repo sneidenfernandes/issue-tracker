9166af7821de3fbb86bca4559ec3711a6a6f028e
sneidenfernandes <sneiden@gmail.com>
2025-04-16 22:57:27 +0100

    Navbar

---
+++ app/Navbar.tsx
@@ -0,0 +1,34 @@
+"use client";
+import React from 'react'
+import Link from 'next/link'
+import { IoIosBug } from "react-icons/io";
+import { usePathname } from 'next/navigation';
+import classnames from "classnames";
+
+const Navbar = () => {
+    const links = [
+        {label: "Dashboard", href: "/"},
+        {label: "Issues", href: "/issues"}
+    ]
+
+    const pathname = usePathname();
+
+  return (
+    <nav className='flex space-x-6 mb-5 border-b h-14 items-center px-5'>
+        <Link href="/">
+            <IoIosBug/>
+        </Link>
+        <ul className='flex space-x-6'>
+            {links.map(link => <li>
+                <Link href={link.href} className={classnames({
+                    "text-zinc-900" : link.href === pathname,
+                    "text-zinc-500" : link.href !== pathname,
+                    "hover:text-zinc-800 transition-colors": true
+                })}>{link.label}</Link>
+            </li>)}
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
